23c929048a39d68d4559ac7f3628810c8d9c16a2
Garblesnarff <wonky.coin@gmail.com>
2025-04-26 20:43:05 -0500

    Fix base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rob-hanson-portfolio/',
+  base: '/website_resume/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
